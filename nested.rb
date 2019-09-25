696776fb2bfbe4a377570addcadbd705a57a946e
Parker Bacall <pbacall2@gmail.com>
2019-09-25 02:35:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,6 @@ def hopper
      }
 
 programmer_hash[:grace_hopper]
-
 end
 
 def alan_kay_is_known_for
@@ -38,7 +37,6 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-		 programmer_hash[:grace_hopper]
 end
 
 def dennis_ritchies_language
